763f35eaaf8b50d8cb103db8cae5c29cf3a0e2f9
谢懿翔 <1240214244@qq.com>
2024-01-10 13:22:13 +0800

    上传项目

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    public event EventHandler OnInteractAction;
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+
+        playerInputActions.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
+
+    /// <summary>
+    /// 通过输入获取移动向量，并标准化
+    /// </summary>
+    /// <returns>返回移动向量</returns>
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        //将输入的向量标准化（同时按下W和D，会输出（1，1），我希望角色的速度不会变，应为（0.71，0.71））
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
+}
