7ad30da7df3df4f9c8eda555b7752dc166de9a1a
谢懿翔 <1240214244@qq.com>
2024-02-21 11:38:11 +0800

    切割柜台输入物品和输出物品完善

--- Assets/Scripts/Kitchen/CuttingCounter.cs
+++ Assets/Scripts/Kitchen/CuttingCounter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO cutKitchenObjectSo;
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
 
     public override void Interact(Player player)
     {
@@ -12,7 +12,11 @@ public class CuttingCounter : BaseCounter
         {
             if (player.HasKitchenObject()) // 玩家拿着KitchenObject
             {
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                // 如果玩家持有的物品可以被切割才可以放上去
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSo()))
+                {
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
             }
             else // 玩家没有拿着KitchenObject
             {
@@ -32,11 +36,39 @@ public class CuttingCounter : BaseCounter
 
     public override void InteractAlternate(Player player)
     {
-        if (HasKitchenObject()) // 柜台有KitchenObject
+        // 柜台有KitchenObject并且可以被切割
+        if (HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSo()))
         {
+            KitchenObjectSO outputKitchenObjectSo = GetOutputForInput(GetKitchenObject().GetKitchenObjectSo());
             GetKitchenObject().DestroySelf();
 
-            KitchenObject.SpawnKitchenObject(cutKitchenObjectSo, this);
+            KitchenObject.SpawnKitchenObject(outputKitchenObjectSo, this);
         }
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray)
+        {
+            if (cuttingRecipeSO.input == inputKitchenObjectSO)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray)
+        {
+            if (cuttingRecipeSO.input == inputKitchenObjectSO)
+            {
+                return cuttingRecipeSO.output;
+            }
+        }
+
+        return null;
     }
 }
