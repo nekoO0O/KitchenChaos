71e30ce0b15733b6947cbcb58b613712986e2ba6
谢懿翔 <1240214244@qq.com>
2024-02-22 22:11:07 +0800

    增加盘子的视觉元素

---
+++ Assets/Scripts/Kitchen/PlateCompleteVisual.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCompleteVisual : MonoBehaviour
+{
+    [Serializable]
+    public struct KitchenObjectSO_GameObject
+    {
+        public KitchenObjectSO kitchenObjectSo;
+        public GameObject gameObject;
+    }
+
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private List<KitchenObjectSO_GameObject> kitchenObjectSoGameObjectsList;
+
+    private void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+
+        foreach (KitchenObjectSO_GameObject kitchenObjectSoGameObject in kitchenObjectSoGameObjectsList)
+        {
+            kitchenObjectSoGameObject.gameObject.SetActive(false);
+        }
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        foreach (KitchenObjectSO_GameObject kitchenObjectSoGameObject in kitchenObjectSoGameObjectsList)
+        {
+            if (kitchenObjectSoGameObject.kitchenObjectSo == e.kitchenObjectSO)
+            {
+                kitchenObjectSoGameObject.gameObject.SetActive(true);
+            }
+        }
+    }
+}
